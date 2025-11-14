88696b1574f311e6e8b97ef5875a0ab9fa515e59
FEAger13 <guli.fan.mult.2020@gmail.com>
2025-11-14 14:02:08 +0500

    Delete my_reminder_bot directory

--- my_reminder_bot/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL", "") + "/webhook"
-PORT = int(os.getenv("PORT", 5000))
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///reminders.db")
